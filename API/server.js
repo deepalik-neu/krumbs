2adc83832b54f5cf60601386c1a28b81f8950df6
Deepali Kasture <kasture.d@northeastern.edu>
2021-12-01 20:47:49 -0500

    Adding port for heroku

--- API/server.js
+++ API/server.js
@@ -3,6 +3,8 @@ import app from  './services/app.js';
 const port = 7001 // PORT SET UP ON 7000 
  
 
-app.listen(port, () => {
+app.listen({
+  port: (process.env.PORT || port)
+}, () => {
   console.log(`App listening at http://localhost:${port}`) //PORT ACTIVE
 })
